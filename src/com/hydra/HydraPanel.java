8eb8ed8fcba88662faf8f8dcf6b52d2146699c24
WeFknRise <Jarno.Michiels@student.pxl.be>
2015-10-04 13:24:18 +0200

    Test

--- src/com/hydra/HydraPanel.java
+++ src/com/hydra/HydraPanel.java
@@ -26,7 +26,7 @@ public class HydraPanel extends JFrame {
 
 	public HydraPanel() {
 
-		this.setTitle("Hail Hydra");
+		this.setTitle("Hail Hydra!!!");
 
 		setVisible(true);
 		setSize(300, 300);
@@ -92,7 +92,7 @@ public class HydraPanel extends JFrame {
 		}
 
 		public void windowClosing(WindowEvent e) {
-			for (int i = 0; i < 2; i++) {
+			for (int i = 0; i < 3; i++) {
 				HydraPanel h = new HydraPanel();
 				positionFrameRandomly(h);
 			}
